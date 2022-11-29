0c1d998a7bad25debf0dce27cb2e104f3a809d48
danilbushkov <danil1357997@gmail.com>
2022-11-29 11:54:06 +0300

    added save, check and remove token

--- backend/user/password.go
+++ backend/user/password.go
@@ -1,6 +1,13 @@
 package user
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"backend/db"
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+const ActivityInterval = 6000
 
 func GetHashedPassword(password string) string {
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
@@ -13,4 +20,37 @@ func GetHashedPassword(password string) string {
 func CheckPassword(password string, hashedPassword string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
 	return err == nil
+}
+
+func SaveToken(token string) {
+	client := db.Redis.Get()
+	defer client.Close()
+
+	_, err := client.Do("SET", token, "", "EX", ActivityInterval)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CheckToken(token string) bool {
+	client := db.Redis.Get()
+	defer client.Close()
+
+	value, err := client.Do("EXISTS", token)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return value.(int64) == 1
+
+}
+
+func RemoveToken(token string) {
+	client := db.Redis.Get()
+	defer client.Close()
+
+	_, err := client.Do("DEL", token)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
